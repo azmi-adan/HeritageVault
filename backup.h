981deca280d4e141de0cadaad148b51adbcea34d
azmi adan <azmi.adan2032@gmail.com>
2025-11-09 20:04:13 +0300

    merged into main.c

--- backup.h
+++
@@ -1,6 +0,0 @@
-#ifndef BACKUP_H
-#define BACKUP_H
-
-void autoBackup();
-
-#endif
--- file_handling.h
+++
@@ -1,9 +0,0 @@
-#ifndef FILE_HANDLING_H
-#define FILE_HANDLING_H
-
-#include "heritage.h"
-
-int saveHeritageItem(HeritageItem* item);
-int loadAllHeritageItems(HeritageItem** items);
-
-#endif
